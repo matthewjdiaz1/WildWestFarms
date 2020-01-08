4c3bde358e5294085b9bbb9b4c26ab13573af2db
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2020-01-07 16:21:33 -0800

    rendered react app

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div>
+        from React
+      </div>
+    )
+  }
+}
+
+export default App;
